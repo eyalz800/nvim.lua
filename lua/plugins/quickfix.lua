e96486e6220ba1acf5e75a19f581b1bf4400d6ce
eyalz800 <eyalz800@gmail.com>
2023-08-20 22:34:00 +0300

    Disable quickfix onopen due to issues

--- lua/plugins/quickfix.lua
+++ lua/plugins/quickfix.lua
@@ -1,22 +1,6 @@
 local m = {}
-local v = require 'vim'
-local cmd = require 'vim.cmd'.silent
-local file_explorer = require 'plugins.file_explorer'
-
-local getwininfo = v.fn.getwininfo
-local win_getid = v.fn.win_getid
 
 m.on_open = function()
-    local info = getwininfo(win_getid())[1]
-    if info.loclist ~= 1 then
-        cmd 'wincmd J'
-        if file_explorer.is_open() then
-            v.defer_fn(function()
-                file_explorer.close()
-                file_explorer.open({ focus=false })
-            end, 0)
-        end
-    end
 end
 
 return m
