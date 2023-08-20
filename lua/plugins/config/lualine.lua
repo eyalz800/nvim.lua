94dccc1dfde29bfe4dc76af9489190e979d0818f
eyalz800 <eyalz800@gmail.com>
2023-08-20 22:33:54 +0300

    Lua add relative path

--- lua/plugins/config/lualine.lua
+++ lua/plugins/config/lualine.lua
@@ -8,7 +8,7 @@ m.options = {
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
             statusline = {},
-            winbar = {},
+            winbar = { 'NvimTree', 'nerdtree' },
         },
         ignore_focus = {},
         always_divide_middle = true,
@@ -22,7 +22,7 @@ m.options = {
     sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+        lualine_c = { { 'filename', path = 1 } },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' }
@@ -39,7 +39,7 @@ m.options = {
     winbar = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {'filename'},
+        lualine_c = { {'filename', path = 1 } },
         lualine_x = {},
         lualine_y = {},
         lualine_z = {}
@@ -47,7 +47,7 @@ m.options = {
     inactive_winbar = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {'filename'},
+        lualine_c = { {'filename', path = 1 } },
         lualine_x = {},
         lualine_y = {},
         lualine_z = {}
