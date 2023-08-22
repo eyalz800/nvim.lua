098cec09a0d8e41be66c525f9f8ec2df8afd4068
eyalz800 <eyalz800@gmail.com>
2023-08-22 22:19:55 +0300

    Buffer and path completions.

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -66,6 +66,8 @@ m.plugins = {
                     'L3MON4D3/LuaSnip',
                     'saadparwaiz1/cmp_luasnip',
                     'hrsh7th/cmp-nvim-lsp',
+                    'hrsh7th/cmp-path',
+                    'hrsh7th/cmp-buffer',
                     {
                         'eyalz800/friendly-snippets',
                         cond = user.settings.lsp_config.nvim.servers.snippets,
