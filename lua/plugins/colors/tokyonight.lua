cee4518434072a90565f5f81442e9b0b2af61214
eyalz800 <eyalz800@gmail.com>
2023-08-22 16:56:41 +0300

    Disable bold and italics.

--- lua/plugins/colors/tokyonight.lua
+++ lua/plugins/colors/tokyonight.lua
@@ -116,9 +116,36 @@ end
 
 m.config = function()
     return {
-        on_highlights = function(hi, colors)
-            hi['@variable.builtin'] = { fg = colors.orange }
-            hi['FloatBorder'] = { fg = colors.fg }
+        style = "storm",
+        light_style = "day",
+        transparent = false,
+        terminal_colors = true,
+        styles = {
+            comments = { italic = false },
+            keywords = { italic = false },
+            functions = {},
+            variables = {},
+            sidebars = "dark",
+            floats = "dark",
+        },
+        sidebars = { "qf", "help" },
+        day_brightness = 0.3,
+        hide_inactive_statusline = false,
+        dim_inactive = false,
+        lualine_bold = false,
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors)
+            highlights['@variable.builtin'] = { fg = colors.orange }
+            highlights['FloatBorder'] = { fg = colors.fg }
         end
     }
 end
