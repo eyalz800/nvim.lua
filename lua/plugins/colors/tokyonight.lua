2c8ed5b15b56ffd637d427909b5b164919a5f028
eyalz800 <eyalz800@gmail.com>
2023-08-22 22:56:08 +0300

    Lsp float border

--- lua/plugins/colors/tokyonight.lua
+++ lua/plugins/colors/tokyonight.lua
@@ -133,19 +133,10 @@ m.config = function()
         hide_inactive_statusline = false,
         dim_inactive = false,
         lualine_bold = false,
-
-        --- You can override specific color groups to use other groups or a hex color
-        --- function will be called with a ColorScheme table
-        ---@param colors ColorScheme
-        on_colors = function(colors) end,
-
-        --- You can override specific highlights to use other groups or a hex color
-        --- function will be called with a Highlights and ColorScheme table
-        ---@param highlights Highlights
-        ---@param colors ColorScheme
+        --on_colors = function(colors) end,
         on_highlights = function(highlights, colors)
             highlights['@variable.builtin'] = { fg = colors.orange }
-            highlights['FloatBorder'] = { fg = colors.fg }
+            highlights['FloatBorder'] = { fg = colors.fg, bg = colors.bg_dark }
         end
     }
 end
