e80620ecded8e3904436f4dae3b747043f8a6d14
eyalz800 <eyalz800@gmail.com>
2023-08-26 17:34:50 +0300

    Removed unneeded path installation

--- lua/main/path.lua
+++ lua/main/path.lua
@@ -1,6 +1,5 @@
 local m = {}
 local v = require 'vim'
-local executable = require 'vim.executable'.executable
 
 local stdpath = v.fn.stdpath
 
@@ -9,13 +8,5 @@ local data_path = stdpath 'data'
 v.env.PATH = data_path .. '/installation/bin/programs' .. ':' ..
              data_path .. '/mason/bin' .. ':' ..
              v.env.PATH
-
-if not executable('clangd') and v.loop.fs_stat('/usr/local/opt/llvm/bin/clangd') then
-    v.env.PATH = v.env.PATH .. '/usr/local/opt/llvm/bin'
-end
-
-if not executable('clangd') and v.loop.fs_stat('/opt/homebrew/opt/llvm/bin/clangd') then
-    v.env.PATH = v.env.PATH .. '/opt/homebrew/opt/llvm/bin'
-end
 
 return m
