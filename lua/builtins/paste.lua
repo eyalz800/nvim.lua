2b74fbb7474844fcc08e40df1bfab64b613e38e6
eyalz800 <eyalz800@gmail.com>
2023-08-28 02:00:31 +0300

    Fixed paste mode

--- lua/builtins/paste.lua
+++ lua/builtins/paste.lua
@@ -1,24 +1,34 @@
 local m = {}
 local v = require 'vim'
-local cmd = require 'vim.cmd'.silent
 
-local sign_column = v.opt.signcolumn
-local mouse = v.opt.mouse
-
-m.on = false
+m.status = { on = false }
 
 m.toggle = function()
-    if not m.on then
-        m.on = true
+    if not m.status.on then
+        m.status = {
+           on = false,
+           signcolumn = v.o.signcolumn,
+           statuscolumn = v.o.statuscolumn,
+           mouse = v.o.mouse,
+           number = v.o.number,
+           relativenumber = v.o.relativenumber,
+        }
         v.opt.signcolumn = 'no'
+        v.opt.statuscolumn = ''
         v.opt.mouse = ''
+		v.opt.number = false
+		v.opt.relativenumber = false
+        v.opt.paste = true
+        m.status.on = true
     else
-        m.on = false
-        v.opt.signcolumn = sign_column
-        v.opt.mouse = mouse
+        v.opt.signcolumn = m.status.signcolumn
+        v.opt.statuscolumn = m.status.statuscolumn
+        v.opt.mouse = m.status.mouse
+		v.opt.number = m.status.number
+		v.opt.relativenumber = m.status.relativenumber
+        v.opt.paste = false
+        m.status.on = false
     end
-    cmd 'set paste!'
-    cmd 'set number!'
 end
 
 return m
