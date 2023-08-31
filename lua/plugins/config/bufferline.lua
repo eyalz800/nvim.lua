de448e1e5083ef1f7f9c566de7c767601b6d873c
eyalz800 <eyalz800@gmail.com>
2023-09-01 01:27:39 +0300

    Added underline to bufferline

--- lua/plugins/config/bufferline.lua
+++ lua/plugins/config/bufferline.lua
@@ -37,6 +37,7 @@ m.config = function()
             hi def link InitLuaBufferLineVisibleBg Normal
             hi def link InitLuaBufferLineNormalFg NonText
             hi def link InitLuaBufferLineNormalBg CursorLine
+            hi def link InitLuaBufferLineIndicator InitLuaBufferLineSelectedBg
         ]=]
     else
         v.cmd [=[
@@ -48,20 +49,22 @@ m.config = function()
             hi def link InitLuaBufferLineVisibleBg Pmenu
             hi def link InitLuaBufferLineNormalFg NonText
             hi def link InitLuaBufferLineNormalBg CursorLine
+            hi def link InitLuaBufferLineIndicator InitLuaBufferLineSelectedBg
         ]=]
     end
 
-    local offset_sep = { highlight = "InitLuaBufferLineOffsetSep", attribute = "bg" }
+    local offset_sep = { highlight = 'InitLuaBufferLineOffsetSep', attribute = 'bg' }
     if user.settings.file_explorer ~= 'nvim-tree' then
-        offset_sep = { highlight = "Normal", attribute = "bg" }
+        offset_sep = { highlight = 'Normal', attribute = 'bg' }
     end
-    local line_bg = { highlight = "InitLuaBufferLineBg", attribute = "bg" }
-    local selected_fg = { highlight = 'InitLuaBufferLineSelectedFg', attribute = "fg" }
-    local selected_bg = { highlight = 'InitLuaBufferLineSelectedBg', attribute = "bg" }
-    local visible_fg = { highlight = 'InitLuaBufferLineVisibleFg', attribute = "fg" }
-    local visible_bg = { highlight = 'InitLuaBufferLineVisibleBg', attribute = "bg" }
-    local fg = { highlight = 'InitLuaBufferLineNormalFg', attribute = "fg" }
-    local bg = { highlight = "InitLuaBufferLineNormalBg", attribute = "bg" }
+    local line_bg = { highlight = 'InitLuaBufferLineBg', attribute = 'bg' }
+    local selected_fg = { highlight = 'InitLuaBufferLineSelectedFg', attribute = 'fg' }
+    local selected_bg = { highlight = 'InitLuaBufferLineSelectedBg', attribute = 'bg' }
+    local visible_fg = { highlight = 'InitLuaBufferLineVisibleFg', attribute = 'fg' }
+    local visible_bg = { highlight = 'InitLuaBufferLineVisibleBg', attribute = 'bg' }
+    local fg = { highlight = 'InitLuaBufferLineNormalFg', attribute = 'fg' }
+    local bg = { highlight = 'InitLuaBufferLineNormalBg', attribute = 'bg' }
+    local underline_sp = { highlight = 'InitLuaBufferLineIndicator', attribute = 'bg' }
 
     return {
         options = {
@@ -113,59 +116,59 @@ m.config = function()
             offset_separator = { fg = offset_sep, bg = offset_sep},
             separator = { fg = line_bg, bg = bg },
             separator_visible = { fg = line_bg, bg = visible_bg },
-            separator_selected = { fg = line_bg, bg = selected_bg },
+            separator_selected = { fg = line_bg, bg = selected_bg, underline = true, sp = underline_sp },
             trunc_marker = { fg = fg, bg = line_bg },
             background = { fg = fg, bg = bg },
             buffer_visible = { fg = visible_fg, bg = visible_bg },
-            buffer_selected = { fg = selected_fg, bg = selected_bg },
+            buffer_selected = { fg = selected_fg, bg = selected_bg, underline = true, sp = underline_sp },
             duplicate = { fg = fg, bg = bg },
             duplicate_visible = { fg = visible_fg, bg = visible_bg },
-            duplicate_selected = { fg = selected_fg, bg = selected_bg },
+            duplicate_selected = { fg = selected_fg, bg = selected_bg, underline = true, sp = underline_sp },
             close_button = { fg = fg, bg = bg },
             close_button_visible = { fg = visible_fg, bg = visible_bg },
-            close_button_selected = { fg = selected_fg, bg = selected_bg },
+            close_button_selected = { fg = selected_fg, bg = selected_bg, underline = true, sp = underline_sp },
             tab = { fg = fg, bg = bg },
-            tab_selected = { fg = selected_fg, bg = selected_bg },
+            tab_selected = { fg = selected_fg, bg = selected_bg, underline = true, sp = underline_sp },
             tab_separator = { fg = line_bg, bg = bg },
-            tab_separator_selected = { fg = line_bg, bg = selected_bg },
+            tab_separator_selected = { fg = line_bg, bg = selected_bg, underline = true, sp = underline_sp },
             tab_close = { fg = fg, bg = line_bg },
             numbers = { fg = fg, bg = bg },
             numbers_visible = { fg = visible_fg, bg = visible_bg },
-            numbers_selected = { fg = selected_fg, bg = selected_bg },
+            numbers_selected = { fg = selected_fg, bg = selected_bg, underline = true, sp = underline_sp },
 
             modified = { bg = bg },
             modified_visible = { bg = visible_bg },
-            modified_selected = { bg = selected_bg },
+            modified_selected = { bg = selected_bg, underline = true, sp = underline_sp },
             pick = { bg = bg },
             pick_visible = { bg = visible_bg },
-            pick_selected = { bg = selected_bg },
+            pick_selected = { bg = selected_bg, underline = true, sp = underline_sp },
             indicator_visible = { bg = visible_bg },
-            indicator_selected = { bg = selected_bg },
+            indicator_selected = { bg = selected_bg, underline = true, sp = underline_sp },
 
             diagnostic = { bg = bg },
             diagnostic_visible = { bg = visible_bg },
-            diagnostic_selected = { bg = selected_bg },
+            diagnostic_selected = { bg = selected_bg, underline = true, sp = underline_sp },
             hint = { bg = bg },
             hint_visible = { bg = visible_bg },
-            hint_selected = { bg = selected_bg },
+            hint_selected = { bg = selected_bg, underline = true, sp = underline_sp },
             info = { bg = bg },
             info_visible = { bg = visible_bg },
-            info_selected = { bg = selected_bg },
+            info_selected = { bg = selected_bg, underline = true, sp = underline_sp },
             info_diagnostic = { bg = bg },
             info_diagnostic_visible = { bg = visible_bg },
-            info_diagnostic_selected = { bg = selected_bg },
+            info_diagnostic_selected = { bg = selected_bg, underline = true, sp = underline_sp },
             warning = { bg = bg },
             warning_visible = { bg = visible_bg },
-            warning_selected = { bg = selected_bg },
+            warning_selected = { bg = selected_bg, underline = true, sp = underline_sp },
             warning_diagnostic = { bg = bg },
             warning_diagnostic_visible = { bg = visible_bg },
-            warning_diagnostic_selected = { bg = selected_bg },
+            warning_diagnostic_selected = { bg = selected_bg, underline = true, sp = underline_sp },
             error = { bg = bg },
             error_visible = { bg = visible_bg },
-            error_selected = { bg = selected_bg },
+            error_selected = { bg = selected_bg, underline = true, sp = underline_sp },
             error_diagnostic = { bg = bg },
             error_diagnostic_visible = { bg = visible_bg },
-            error_diagnostic_selected = { bg = selected_bg },
+            error_diagnostic_selected = { bg = selected_bg, underline = true, sp = underline_sp },
         },
     }
 end
