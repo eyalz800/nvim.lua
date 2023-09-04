7dfbb965f13a29f1f9c65bb8810adf4f556ca7e1
eyalz800 <eyalz800@gmail.com>
2023-09-04 18:59:10 +0300

    Add quit to fugitive

--- lua/main/autocmd.lua
+++ lua/main/autocmd.lua
@@ -95,7 +95,8 @@ end
 
 autocmd({'WinEnter'}, {pattern='term://*', group=augroup('init.lua.term.enter', {}), command='startinsert'})
 
-autocmd('filetype', {pattern='fugitiveblame', group=augroup('init.lua.fugitive-diffview', {}), command=[=[nnoremap <buffer> <silent> d <cmd>silent exec 'norm! 0eb"xyw' <bar> wincmd l <bar> silent exec 'DiffviewFileHistory % --range='.getreg('x')<cr>]=]})
+autocmd('filetype', {pattern='fugitiveblame', group=augroup('init.lua.fugitive-diffview', {}), command=
+    [=[nnoremap <buffer> <silent> q <cmd>q<cr> | nnoremap <buffer> <silent> d <cmd>silent exec 'norm! 0eb"xyw' <bar> wincmd l <bar> silent exec 'DiffviewFileHistory % --range='.getreg('x')<cr>]=]})
 
 if user.settings.bar == 'barbecue' and user.settings.lsp == 'nvim' then
     autocmd({ 'WinResized', 'BufWinEnter', 'CursorHold', 'InsertLeave' },
