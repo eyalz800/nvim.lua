bbe3f4f75ea0ab037482d375f7ecf1068494e9df
eyalz800 <eyalz800@gmail.com>
2024-03-03 19:24:07 +0200

    Copilot config

--- lua/plugins/config/copilot.lua
+++ lua/plugins/config/copilot.lua
@@ -6,28 +6,62 @@ m.config = function()
         panel = {
             enabled = true,
             auto_refresh = true,
+            keymap = {
+                jump_prev = '[[',
+                jump_next = ']]',
+                accept = "<CR>",
+                refresh = 'gr',
+                open = "<leader>gp"
+            },
+            layout = {
+                position = 'bottom', -- | top | left | right
+                ratio = 0.4
+            },
         },
         suggestion = {
             enabled = true,
-            -- use the built-in keymapping for "accept" (<M-l>)
             auto_trigger = true,
-            accept = false,         -- disable built-in keymapping
+            debounce = 75,
+            keymap = {
+                accept = '<C-k>',
+                accept_word = false,
+                accept_line = false,
+                next = '<C-h>',
+                prev = '<C-l>',
+                dismiss = '<C-j>',
+            },
         },
+        filetypes = {
+            yaml = false,
+            markdown = false,
+            help = false,
+            gitcommit = false,
+            gitrebase = false,
+            hgcommit = false,
+            svn = false,
+            cvs = false,
+            ['.'] = false,
+        },
+        copilot_node_command = 'node', -- Node.js version must be > 18.x
+        server_opts_overrides = {},
     }
 end
 
 m.setup = function()
-    -- hide copilot suggestions when cmp menu is open
-    -- to prevent odd behavior/garbled up suggestions
-    local cmp_status_ok, cmp = pcall(require, 'cmp')
-    if cmp_status_ok then
-        cmp.event:on('menu_opened', function()
-            v.b.copilot_suggestion_hidden = true
-        end)
+    local hide_on_cmp = false
+    if hide_on_cmp then
+        -- hide copilot suggestions when cmp menu is open
+        -- to prevent odd behavior/garbled up suggestions
+        local cmp_status_ok, cmp = pcall(require, 'cmp')
+        if cmp_status_ok then
+            cmp.event:on('menu_opened', function()
+                v.b.copilot_suggestion_hidden = true
+            end)
 
-        cmp.event:on('menu_closed', function()
-            v.b.copilot_suggestion_hidden = false
-        end)
+            cmp.event:on('menu_closed', function()
+                v.b.copilot_suggestion_hidden = false
+            end)
+        end
     end
 end
 
