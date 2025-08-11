18f4236f62fac7aae48ddbd9ac02c76aef7a7eee
eyalz800 <eyalz800@gmail.com>
2025-08-11 18:50:42 +0300

    Mini files mappings

--- lua/plugins/config/mini-files.lua
+++ lua/plugins/config/mini-files.lua
@@ -2,19 +2,33 @@ local m = {}
 
 m.setup = function()
     local mini_files = require 'mini.files'
+    vim.api.nvim_create_autocmd('filetype', {
+        group = vim.api.nvim_create_augroup('init.lua.plugins.mini-files', {}),
+        pattern = 'minifiles',
+        callback = function()
+            vim.keymap.set({ 'n', 'i' }, '<c-s>', mini_files.synchronize, { buffer = true, desc = 'Synchronize mini file explorer' })
+        end,
+    })
     mini_files.setup(m.config())
-    vim.keymap.set('n', '<c-w>f', '<cmd>lua MiniFiles.open()<CR>', { desc = 'Toggle mini file explorer' })
+    vim.keymap.set('n', '<c-w>f', mini_files.open, { desc = 'Open mini file explorer' })
     vim.keymap.set('n', '<c-w>u', function()
-        mini_files.open(vim.api.nvim_buf_get_name(0), false)
+        local current_file_path = vim.api.nvim_buf_get_name(0)
+        local ok, _ = pcall(mini_files.open, current_file_path, false)
+        if not ok then
+            ok, _ = pcall(mini_files.open, vim.fs.dirname(current_file_path), false)
+            if not ok then
+                mini_files.open()
+            end
+        end
         mini_files.reveal_cwd()
-    end, { desc = "Toggle into currently opened file" })
+    end, { desc = 'Open mini file explorer and focus on current file' })
 end
 
 m.config = function()
     return {
         mappings = {
-            go_in = '<CR>',
-            go_in_plus = 'L',
+            go_in = 'L',
+            go_in_plus = '<cr>',
             go_out = '-',
             go_out_plus = 'H',
         },
