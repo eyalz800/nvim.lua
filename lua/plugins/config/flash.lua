7f8d4c8d5272c45cbbc3787e98f031ccb1794af2
eyalz800 <eyalz800@gmail.com>
2025-08-13 23:01:16 +0300

    Flash var

--- lua/plugins/config/flash.lua
+++ lua/plugins/config/flash.lua
@@ -1,6 +1,9 @@
 local m = {}
+local flash = {}
+
 m.setup = function()
-    require 'flash'.setup(m.config())
+    flash = require 'flash'
+    flash.setup(m.config())
 end
 
 m.config = function()
@@ -263,12 +266,11 @@ m.config = function()
 end
 
 m.keys = {
-    { "s",     mode = { "n", "o", "x" }, function() require("flash").jump() end,              desc = "Flash" },
-    { "S",     mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-    { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-    { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc =
-    "Toggle Flash Search" },
+    { "s",     mode = { "n", "o", "x" }, function() flash.jump() end,              desc = "Flash" },
+    { "S",     mode = { "n", "o", "x" }, function() flash.treesitter() end,        desc = "Flash Treesitter" },
+    { "r",     mode = "o",               function() flash.remote() end,            desc = "Remote Flash" },
+    { "R",     mode = { "o", "x" },      function() flash.treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" },           function() flash.toggle() end,            desc = "Toggle Flash Search" },
 }
 
 return m
