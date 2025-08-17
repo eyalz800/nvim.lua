5636b79b473bff3358642901d083b292426c23a0
eyalz800 <eyalz800@gmail.com>
2025-08-17 10:22:00 +0300

    Correctly remove refresh trigger

--- lua/plugins/indent-guides.lua
+++ lua/plugins/indent-guides.lua
@@ -1,7 +1,32 @@
 local m = {}
+-- local cmd = require 'vim.cmd'.silent
+-- local exists = require 'vim.exists'.exists
+local feed_keys = require 'vim.feed-keys'.feed_keys
+-- local mode = vim.fn.mode
+
+-- local timer = vim.loop.new_timer()
 
 m.refresh_trigger = function(source, options)
-    return function() end
+    options = options or {}
+    return function()
+        -- timer:stop()
+        --
+        -- timer:start(0, 250, vim.schedule_wrap(function()
+        --     local current_mode = mode()
+        --     if current_mode == 'n' or current_mode == 'v' then
+        --         if exists ':IndentBlanklineRefresh' then
+        --             cmd 'IndentBlanklineRefresh'
+        --         end
+        --         timer:stop()
+        --     end
+        -- end))
+
+        if not options.expr then
+            feed_keys(source)
+        else
+            return source
+        end
+    end
 end
 
 return m
