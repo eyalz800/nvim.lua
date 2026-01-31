9ea37f0210f23c4cdbc498568d3089d511702e32
eyalz800 <eyalz800@gmail.com>
2026-01-31 23:07:15 +0200

    Catch errors from serverstart

--- lua/plugins/godot.lua
+++ lua/plugins/godot.lua
@@ -3,7 +3,7 @@ local m = {}
 m.setup = function()
     local project_file = vim.fn.getcwd() .. '/project.godot'
     if vim.fn.filereadable(project_file) ~= 0 then
-        vim.fn.serverstart('./godothost')
+        pcall(vim.fn.serverstart, './godothost')
     end
 end
 
