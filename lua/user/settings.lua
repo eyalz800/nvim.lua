c546ab8fc4c7e5fbf6567236c933b36255f5983e
eyalz800 <eyalz800@gmail.com>
2026-02-01 01:39:56 +0200

    Update clang version

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -16,7 +16,7 @@ m.colorscheme = vim.fn.system('if [ -e ~/.tmux.color ] ; then cat ~/.tmux.color 
 m.colorscheme_settings = { tokyonight = { style = 'storm' } }
 m.powerline = true
 m.icons = 'nvim-web-devicons'
-m.install_options = { clang_version = 20 }
+m.install_options = { clang_version = 21 }
 m.unmodifiable_insert_append_jump = true
 m.finder = 'fzf-lua'
 m.line = 'lualine'
